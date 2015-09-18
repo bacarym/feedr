99bb0e6aee0e7c4bed7ba68ed2d0b1a943147c8f
bacary mbaye <bacarymbaye@MacBook-Pro-de-bacary-2.local>
2015-09-18 13:58:16 +0200

    then pb

--- www/src/pages/home.controller.js
+++ www/src/pages/home.controller.js
@@ -1,23 +1,69 @@
 "use strict";
 
-angular.module('starter').controller("FeedController", function($http, $scope) {
+angular.module('starter').controller("FeedController", function($http, $scope,$q) {
  
+//list = ["http://www.frenchweb.fr/feed/","http://feeds.feedburner.com/Maddyness","http://www.emarketinglicious.fr/feed"]
+
+    var list = [
+            {
+                name:'Frenchweb',
+                url: "http://www.frenchweb.fr/feed/",
+                entries:[]
+            },
+            {
+                name:'Maddyness',
+                url: "http://feeds.feedburner.com/Maddyness",
+                entries:[]
+
+            },
+            {
+                name:'Emarketinglicious',
+                url: "http://www.emarketinglicious.fr/feed",
+                entries:[]
+            },
+            ];
+
+
     $scope.init = function() {
 
-        $http.jsonp("//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=" + 10 + "&callback=JSON_CALLBACK&q="+"http://www.frenchweb.fr/feed/")
 
-        //$http.get("http://ajax.googleapis.com/ajax/services/feed/load", { params: { "v": "1.0", "q": "http://www.frenchweb.fr/actualite/" } })
-            .success(function(data) {
+
+        for (var i = 0; i < list.length; i++) { 
+
+            var url = list[i].url;
+
+            var promise = $q.defer();
+        list[i].entries =  $http.jsonp("//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=" + 10 + "&callback=JSON_CALLBACK&q="+url)
+         .success(function(data) {
                 
-                console.log(data)
-                $scope.rssTitle = data.responseData.feed.title;
-                $scope.rssUrl = data.responseData.feed.feedUrl;
-                $scope.rssSiteUrl = data.responseData.feed.link;
-                $scope.entries = data.responseData.feed.entries;
-            })
-            .error(function(data) {
+            
+                //$scope.rssTitle = data.responseData.feed.title;
+                //$scope.rssUrl = data.responseData.feed.feedUrl;
+                //$scope.rssSiteUrl = data.responseData.feed.link;
+     console.log(" data.responseData.feed.entries: " ,  data.responseData.feed);
+                 promise.resolve(data.responseData);
+                 return promise;
+                
+            }).then(function(data){
+                console.log("then ",data.responseData);
+         });
+            /*.catch(function(data) {
                 console.log("ERROR: " + data);
-            });
+            });*/
+
+            console.log(list[i].entries);
+
+        
+      
+
+
+        }
+
+              $scope.entries = list;
+
+
+
+
     }
 
    
